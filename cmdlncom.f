9a9dc0f9d3214c2ffe30a5cc6743890ca09bb7c8
Ian Hutchinson <ihutch@mit.edu>
2015-10-14 13:36:19 -0400

    Not sure what the changes were. But committing before additional.

--- cmdlncom.f
+++ cmdlncom.f
@@ -1,13 +1,16 @@
 
       integer ntmax      
       parameter (ntmax=100)
-      integer ntroff,nmask,jt1,ism,iticnum,markoff,jj
-      logical lx,mx,ly,my,la,lw,lp,lj
+      integer ntroff,nmask,jt1,ism,iticnum,markoff,jj,nannot
+      integer ixof,iyof,ixsc,iysc,nstycyc,ncolumns
+      logical lx,mx,ly,my,la,lw,lp,lj,ll
       real xlego,ylego,xrange,yrange,xsf,ysf,xof,yof,ysxp,cz,ybh
       character*256 ytitle,xtitle
       dimension nmask(ntmax),xrange(2),yrange(2)
+      real xrit(2,ntmax),yrit(2,ntmax)
       
       common /cmdlncom/ ntroff,nmask,jt1,ism,iticnum,markoff,jj,
-     $     lx,mx,ly,my,la,lw,lp,lj,
+     $     lx,mx,ly,my,la,lw,lp,lj,ll,nannot,
+     $     ixof,iyof,ixsc,iysc,nstycyc,ncolumns,
      $     xlego,ylego,xrange,yrange,xsf,ysf,xof,yof,ysxp,cz,ybh,
-     $     ytitle,xtitle
+     $     ytitle,xtitle,xrit,yrit
--- plottraces.f
+++ plottraces.f
@@ -18,12 +18,14 @@ c are sent to the argument processor.
       integer npmax
       parameter (npmax=5000)
       logical lpm(ntmax),ljm(ntmax)
-      character*256 legends(ntmax)
+      character*256 legends(ntmax),annotations(ntmax)
       real y(npmax,ntmax)
       real x(npmax,ntmax)
       integer np(ntmax),ntraceno(ntmax),ifirst
+      real yleginc
       data ifirst/1/
       data legends/ntmax*' '/
+      data yleginc/0./
 
       il=0
       jj=1
@@ -44,11 +46,12 @@ c Non-switch argument is assumed to be a filename for reading.
 c Read lines from the data file. They can be of the form
 c :-sx which acts just like cmdline switches.
             call linereading(il,lpm,ljm,legends,y,x,npmax,np,ntraceno,nt
-     $           ,argstr,charin,filename)
+     $           ,argstr,charin,filename,annotations)
             close(13)
-         endif
+         else
 c Intepret switch in cmdline.
-         call cmdlineinterp(argstr)
+            call cmdlineinterp(argstr)
+         endif
       enddo
 c---------------------------------------------
 c End of reading command line arguments do loop.
@@ -64,82 +67,88 @@ c End of reading command line arguments do loop.
          i1=1
          i2=1
       endif
+      itic=max(6,iticnum)
       icount=0
+      ioldframe=0
+      maxframe=nmask(jj-1)
+      if(maxframe.gt.1)call multiframe(maxframe,ncolumns,1)
+c Do over the traces:
       do it=i1,jj-1,i2
          icount=icount+1
-         if(ifirst.eq.1.and.nmask(it).ne.0)then
-            if(ybh.ne.0.)call axregion(0.31,0.91,.1,.1+ybh)
-            if(.not.(xrange(1).eq.0. .and. xrange(2).eq.0.))then
-               if(yrange(1).eq.0. .and. yrange(2).eq.0.)then
-                  call minmax(y(1,it),np(1),yrange(1),yrange(2))
-               endif
-               if(la)then
-                  call pltinaspect(xrange(1),xrange(2),yrange(1)
-     $                 ,yrange(2))
+         if(nmask(it).ne.0)then
+            if(ioldframe.ne.nmask(it))then
+               if(ioldframe.ne.0)call color(15)
+               yleginc=(-(icount+markoff-1))*.05*cz
+               if(ylego.eq.0..and.maxframe.gt.1)yleginc=yleginc+.05*cz
+               if(ybh.ne.0.)call axregion(0.31,0.91,.1,.1+ybh)
+c Possible individual frame plot ranges.
+               if(yrit(1,nmask(it)).ne.yrit(2,nmask(it)))then
+c Multi-frame ranges set separately.
+                  call accisinit()
+                  if(xrit(1,nmask(it)).ne.xrit(2,nmask(it)))then
+                     call scalewn(xrit(1,nmask(it)),xrit(2,nmask(it))
+     $                    ,yrit(1,nmask(it)),yrit(2,nmask(it)),lx,ly)
+                  else
+                     if(xrange(1).eq.0. .and. xrange(2).eq.0.)then
+                        call minmax(x(1,it),np(1),xrange(1),xrange(2))
+                        if(.not.lx)call fitrange(xrange(1),xrange(2)
+     $                       ,itic,nxfac,xfac,xdelta,xrange(1)
+     $                       ,xrange(2))
+                     endif
+                     call scalewn(xrange(1),xrange(2),yrit(1,nmask(it))
+     $                    ,yrit(2,nmask(it)),lx,ly)
+                  endif
+               elseif(.not.(xrange(1).eq.0. .and. xrange(2).eq.0.))then
+                  if(yrange(1).eq.0. .and. yrange(2).eq.0.)then
+                     call minmax(y(1,it),np(1),ymin,ymax)
+                     if(.not.ly)call fitrange(ymin,ymax,itic
+     $                    ,nxfac,xfac,xdelta,ymin,ymax)
+                  else
+                     ymin=yrange(1)
+                     ymax=yrange(2)
+                  endif
+                  if(la)then
+                     call pltinaspect(xrange(1),xrange(2),ymin,ymax)
+                  else
+                     call accisinit()
+                  endif
+                  call scalewn(xrange(1),xrange(2),ymin,ymax,lx,ly)
+               elseif(.not.(yrange(1).eq.0. .and. yrange(2).eq.0.))then
+                  if(xrange(1).eq.0. .and. xrange(2).eq.0.)then
+                     call minmax(x(1,it),np(1),xrange(1),xrange(2))
+                     if(.not.lx)call fitrange(xrange(1),xrange(2),itic
+     $                    ,nxfac,xfac,xdelta,xrange(1),xrange(2))
+                  endif
+                  if(la)then
+                     call pltinaspect(xrange(1),xrange(2),yrange(1)
+     $                    ,yrange(2))
+                  else
+                     call accisinit()
+                  endif
+                  call scalewn(xrange(1),xrange(2),yrange(1),yrange(2)
+     $                 ,lx,ly)
                else
-                  call pltinit(xrange(1),xrange(2),yrange(1),yrange(2))
+                  call lautoinit(x(1,it),y(1,it),np(it),lx,ly)
                endif
-               call scalewn(xrange(1),xrange(2),yrange(1),yrange(2),lx
-     $              ,ly)
+               if(cz.ne.1.)call charsize(cz*.015,cz*.015)
                call axis()
+               call axis2()
                call winset(.true.)
-               call color((ntraceno(it)-1)/i2+1)
-               call dashset((ntraceno(it)-1)/i2)
-               if(ljm(it))call polyline(x(1,it),y(1,it),np(it))
-               if(lpm(it))then
-                  call color((ntraceno(it+markoff)-1)/i2+1)
-                  call polymark(x(1,it),y(1,it),np(it),
-     $                 (ntraceno(it+markoff)-1)/i2+1)
-               endif
-            elseif(.not.(yrange(1).eq.0. .and. yrange(2).eq.0.))then
-               if(xrange(1).eq.0. .and. xrange(2).eq.0.)then
-                  call minmax(x(1,it),np(1),xrange(1),xrange(2))
-               endif
-               if(la)then
-                  call pltinaspect(xrange(1),xrange(2),yrange(1)
-     $                 ,yrange(2))
-               else
-                  call pltinit(xrange(1),xrange(2),yrange(1),yrange(2))
-               endif
-               call scalewn(xrange(1),xrange(2),yrange(1),yrange(2),lx
-     $              ,ly)
-               call axis()
-               call winset(.true.)
-               call color((ntraceno(it)-1)/i2+1)
-               call dashset((ntraceno(it)-1)/i2)
-               if(ljm(it))call polyline(x(1,it),y(1,it),np(it))
-               if(lpm(it))then
-                  call color((ntraceno(it+markoff)-1)/i2+1)
-                  call polymark(x(1,it),y(1,it),np(it),
-     $                 (ntraceno(it+markoff)-1)/i2+1)
-               endif
-            else
-               call lautoinit(x(1,it),y(1,it),np(it),lx,ly)
-               call axis()
-               call color((ntraceno(it)-1)/i2+1)
-               call dashset((ntraceno(it)-1)/i2)
-               if(ljm(it))call polyline(x(1,it),y(1,it),np(it))
-               if(lpm(it))then
-                  call color((ntraceno(it+markoff)-1)/i2+1)
-                  call polymark(x(1,it),y(1,it),np(it),
-     $                 (ntraceno(it+markoff)-1)/i2+1)
-               endif
+               ifirst=0
+               ioldframe=nmask(it)
             endif
-            ifirst=0
-         else
+c Plot the actual trace:
             call color((ntraceno(it)-1)/i2+1)
-            call dashset((ntraceno(it)-1)/i2)
-            if(nmask(it).ne.0)then
-               if(ljm(it))call polyline(x(1,it),y(1,it),np(it))
-               if(lpm(it))then
-                  call color((ntraceno(it+markoff)-1)/i2+1)
-                  call polymark(x(1,it),y(1,it),np(it),
-     $                 (ntraceno(it+markoff)-1)/i2+1)
-               endif
+            call dashset(mod((ntraceno(it)-1)/i2,nstycyc))
+            if(ljm(it))call polyline(x(1,it),y(1,it),np(it))
+            if(lpm(it))then
+               call color((ntraceno(it+markoff)-1)/i2+1)
+               call polymark(x(1,it),y(1,it),np(it),
+     $              (ntraceno(it+markoff)-1)/i2+1)
             endif
          endif
          write(*,'(i4,8f8.4)')it,(y(k,it),k=1,8)
-
+c Legends:
          jl=lentrim(legends(icount))
          if(nmask(it).ne.0)then
             if(jl.eq.0)then
@@ -148,29 +157,42 @@ c End of reading command line arguments do loop.
                legends(icount)=argstr(1:iw)//' '//charin(1:iw2)
                jl=lentrim(legends(icount))
             endif
-            if(jl.gt.1)then
+            if(jl.gt.1.and.ll)then
                call winset(.false.)
-            if(lpm(it))then
-               ntr=ntraceno(it+markoff)/i2
-               if(ljm(it))ntr=-ntr
-               call legendline(xlego,(icount+markoff)*.05*cz+ylego
-     $              ,ntr,legends(icount)(1:jl))
-            else
-               call legendline(xlego,icount*.05*cz+ylego,0,
-     $              legends(icount)(1:jl))
-            endif
+               if(lpm(it))then
+                  ntr=ntraceno(it+markoff)/i2
+                  if(ljm(it))ntr=-ntr
+                  call legendline(xlego,(icount+markoff)*.05*cz+ylego
+     $                 +yleginc,ntr,legends(icount)(1:jl))
+               else
+                  call legendline(xlego,icount*.05*cz+ylego+yleginc,0,
+     $                 legends(icount)(1:jl))
+               endif
                call winset(.true.)
             endif
+         else
+            yleginc=yleginc-.05*cz
          endif
       enddo
       write(*,'(a,i3,a)')'Plotted',jj-1,'  traces'
       call color(15)
       if(xtitle(1:1).ne.' '.or.ytitle(1:1).ne.' ')then
          call winset(.false.)
-         call axlabels(xtitle(1:lentrim(xtitle))
-     $        ,ytitle(1:lentrim(ytitle)))
+         if(maxframe.gt.1)then
+            call axlabels(xtitle(1:lentrim(xtitle)),' ')
+            call charangl(90.)
+            call jdrwstr(.03,.35,ytitle(1:lentrim(ytitle)),0.)
+            call charangl(0.)
+         else
+            call axlabels(xtitle(1:lentrim(xtitle))
+     $           ,ytitle(1:lentrim(ytitle)))
+         endif
       endif
-      call axis2()
+c Deal with annotations:
+      do i=1,nannot
+         read(annotations(i),*)xa,ya,argstr
+         call drwstr(xa,ya,argstr(1:lentrim(argstr)))
+      enddo
 
       call pltend
 
@@ -204,8 +226,10 @@ c can become problematic.
       implicit none
       character*(*) argstr
       include 'cmdlncom.f'
+      external cmdlnbd
 
-      integer j
+      integer j,k
+
 c Entry point of switch interpretation:
       if(argstr(1:3) .eq. '-lx') lx=.not.lx
       if(argstr(1:3) .eq. '-mx') mx=.not.mx
@@ -217,6 +241,7 @@ c Entry point of switch interpretation:
       if(argstr(1:2) .eq. '-v') lw=.true.
       if(argstr(1:2) .eq. '-p') lp=.not.lp
       if(argstr(1:2) .eq. '-j') lj=.not.lj
+      if(argstr(1:3) .eq. '-nl') ll=.not.ll
       if(argstr(1:2) .eq. '-z')then
          read(argstr(3:),'(i5)',end=108,err=108)ntroff
          ntroff=-ntroff+jj-1
@@ -226,8 +251,17 @@ c Entry point of switch interpretation:
          write(*,*)'ntroff=',ntroff
       endif
       if(argstr(1:2) .eq. '-t')then
-         read(argstr(3:),'(256i1)',end=107)nmask
- 107     write(*,'(a,10i1)')'Mask starts ',(nmask(j),j=1,10)
+         if(argstr(3:3).eq.'y')then
+            read(argstr(4:),*,end=110,err=110)((yrit(k,j),k=1,2),j=1
+     $           ,ntmax)
+ 110        continue
+            write(*,'(a,6f8.4)')'Yranges',((yrit(k,j),k=1,2),j=1,3)
+         elseif(argstr(3:3).eq.'n')then
+            read(argstr(4:),*)ncolumns
+         else
+            read(argstr(3:),'(256i1)',end=107)nmask
+ 107        write(*,'(a,10i1)')'Mask starts ',(nmask(j),j=1,10)
+         endif
       endif
       if(argstr(1:3) .eq. '-st')read(argstr(4:),*)jt1
       if(argstr(1:3) .eq. '-lo')read(argstr(4:),*)xlego,ylego
@@ -238,10 +272,15 @@ c Entry point of switch interpretation:
       if(argstr(1:3) .eq. '-sm') read(argstr(4:),*)ism
       if(argstr(1:3) .eq. '-ox') read(argstr(4:),*)xof
       if(argstr(1:3) .eq. '-oy') read(argstr(4:),*)yof
+      if(argstr(1:3) .eq. '-oi') read(argstr(4:),*)ixof
+      if(argstr(1:3) .eq. '-oj') read(argstr(4:),*)iyof
+      if(argstr(1:3) .eq. '-si') read(argstr(4:),*)ixsc
+      if(argstr(1:3) .eq. '-sj') read(argstr(4:),*)iysc
       if(argstr(1:5) .eq. '-spyx') read(argstr(6:),*)ysxp
       if(argstr(1:3) .eq. '-cz') read(argstr(4:),*)cz
       if(argstr(1:3) .eq. '-nt') read(argstr(4:),*)iticnum
       if(argstr(1:3) .eq. '-mo') read(argstr(4:),*)markoff
+      if(argstr(1:3) .eq. '-ds') read(argstr(4:),*)nstycyc
       if(argstr(1:3) .eq. '-bh') read(argstr(4:),*)ybh
       if(argstr(1:2) .eq. '-?') goto 106
 
@@ -258,19 +297,29 @@ c Usage messages.
       write(*,*)' -st<num> set which one of multiple traces to plot'
       write(*,*)' -sm<num> smooth traces with triangular width <num>'
       write(*,*)' -ox<xof> -oy<yof> offset x or y by values.'
+      write(*,*)' -oi<ixof> -oj<iyof> use x or y difference from'
+     $     ,' their nth value.'
+      write(*,*)' -si<ixsc> -oj<iysc> use x or y normalized to'
+     $     ,' their nth value.'
       write(*,*)' -p toggle points, -j toggle line plotting.'
-     $     ,' -t1001... mask traces.'
+      write(*,*)' -nl toggle legends.'
+      write(*,*)' -t1001... mask traces. -t112233... multiframe traces.'
+      write(*,*)' -ty<y1n>,<y1x>,<y2n>,y2x>... set multiframe y-ranges'
+      write(*,*)' -tn<num> use num columns to shrink width'
       write(*,*)' -v Verbose. Write out args, non-data lines read etc.'
       write(*,*)' -lo<xorig>,<yorig> box fraction origin of legends.'
      $     ,' -cz<size> Character scaling size.'
      $     ,' -nt<itics> set tic-number.'
       write(*,*)' -bh<height> Set height of plot box in normal units.'
       write(*,*)' -mo<int> offset count of marks vs lines/traces.'
+      write(*,*)' -ds<int> set trace cycle number of line dash style.'
       write(*,*
      $     )' -z[ntroff] Offset trace count to 1 [or ntroff]' 
       write(*,*)'In data files,'
      $     ,' the first line starting with an integer is data start.'
       write(*,*)'Prior lines starting legend: define successive legends'
+      write(*,*)'Lines starting annotations: define annotations with '
+     $     ,'  format: (2f10.4,a) x-y norm-position, string.'
       write(*,*)'Lines starting :- are interpreted as extra switches'
 
       call exit(0)
@@ -281,35 +330,42 @@ c*********************************************************************
 c Separated block data program required by standard but not gnu.
       implicit none
       include 'cmdlncom.f'
+      integer nrit
+      parameter(nrit=2*ntmax)
 
       data ly/.false./my/.false./lx/.false./mx/.false./la/.false./
-      data lp/.false./lj/.true./lw/.false./
+      data lp/.false./lj/.true./lw/.false./ll/.true./
       data xrange/0.,0./yrange/0.,0./
       data xtitle/' '/ytitle/' '/
-      data nmask/ntmax*1/
+      data nmask/ntmax*1/ncolumns/1/
       data xlego/.2/ylego/0./cz/1./
       data xsf/1./ysf/1./ybh/0./ysxp/0./xof/0./yof/0./
       data ntroff/0/iticnum/0/
       data markoff/0/ism/0/
+      data nannot/0/
+      data ixof/0/iyof/0/ixsc/0/iysc/0/nstycyc/100/
+      data xrit/nrit*0/yrit/nrit*0/
 
       end
 c*********************************************************************
       subroutine linereading(il,lpm,ljm,legends,y,x,npmax,np,ntraceno,nt
-     $     ,argstr,charin,filename)
+     $     ,argstr,charin,filename,annotations)
 c This routine reads lines from an already opened file and interprets
 c them either by calling the commandline interpreter if they are really
 c switches, or else by its own logic as data lines etc.
 
       implicit none
-      include 'cmdlncom.f'
+      include 'cmdlncom.f' 
 
       character*(*) argstr,charin,filename,legends(ntmax)
+     $     ,annotations(ntmax)
       integer npmax
       logical lpm(ntmax),ljm(ntmax)
       real y(npmax,ntmax)
       real x(npmax,ntmax)
       integer np(ntmax),ntraceno(ntmax)
       integer il,nt
+      real xxd,yyd
 
       integer j,k,ii
       
@@ -319,7 +375,10 @@ c switches, or else by its own logic as data lines etc.
 c Loop till we run out of lines:
  101     read(13,'(a)',end=102)charin
          if(lw)write(*,*)charin(1:lentrim(charin))
-         if(charin(1:7).eq.'legend:')then
+         if(charin(1:11).eq.'annotation:')then
+            nannot=nannot+1
+            read(charin(12:),'(a)')annotations(nannot)
+         elseif(charin(1:7).eq.'legend:')then
 c Legend
             read(charin(8:),'(a)')legends(il)
             if(lentrim(legends(il)).eq.0)legends(il)=char(255)
@@ -329,7 +388,7 @@ c Legend
 c Process a file-line switch
             argstr=charin(2:)
             if(lw)write(*,'(a,a)')
-     $           'File Argument:',argstr(1:lentrim(argstr))
+     $           'File Argument ',argstr(1:lentrim(argstr))
 c by calling the processor.
             call cmdlineinterp(argstr)
          endif
@@ -352,11 +411,11 @@ c Read and process the data for these trace(s).
             read(13,*,end=105,err=105)x(ii,jj),(y(ii,j),j=jj,jj+nt-1)
 c Scaling x,y
             if(xsf.ne.1. .or. xof.ne.0.)then
-               x(ii,jj)=xsf*x(ii,jj)+xof
+               x(ii,jj)=xsf*(x(ii,jj)+xof)
             endif
             if(ysf.ne.1. .or. yof.ne.0.)then
                do j=jj,jj+nt-1
-                  y(ii,j)=ysf*y(ii,j)+yof
+                  y(ii,j)=ysf*(y(ii,j)+yof)
                enddo
             endif
             if(ysxp.ne.0.)then
@@ -376,6 +435,41 @@ c Set x-array the same for multiple y's
                x(ii,j)=x(ii,jj)
             enddo
          enddo
+c Processing of entire traces starts here.
+c Differencing from a particular position's value
+         if(ixof.ne.0)then
+            xxd=x(ixof,jj)
+            do ii=1,np(jj)
+               do j=jj+1,jj+nt-1
+                  x(ii,j)=x(ii,j)-xxd
+               enddo
+            enddo
+         endif
+         if(iyof.ne.0)then
+            do j=jj,jj+nt-1
+               yyd=y(iyof,j)
+               do ii=1,np(jj)
+                  y(ii,j)=y(ii,j)-yyd
+               enddo
+            enddo
+         endif
+c Scaling (normalization) by a particular position's value
+         if(ixsc.ne.0)then
+            xxd=x(ixsc,jj)
+            do ii=1,np(jj)
+               do j=jj+1,jj+nt-1
+                  x(ii,j)=x(ii,j)/xxd
+               enddo
+            enddo
+         endif
+         if(iysc.ne.0)then
+            do j=jj,jj+nt-1
+               yyd=y(iysc,j)
+               do ii=1,np(jj)
+                  y(ii,j)=y(ii,j)/yyd
+               enddo
+            enddo
+         endif
          if(ism.ne.0)then
 c Smooth traces
             do j=jj,jj+nt-1
